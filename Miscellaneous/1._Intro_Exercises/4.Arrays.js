2d8c138bd75a4c7d2c1272b336671ee68165519a
Jacob Uhlig <jacobuhlig@gmail.com>
2023-02-09 15:16:00 +0100

    Finished the 4.Arrays.js exercises.

--- Miscellaneous/1._Intro_Exercises/4.Arrays.js
+++ Miscellaneous/1._Intro_Exercises/4.Arrays.js
@@ -6,22 +6,52 @@
 const letters = ["a","b","c"];
 // show b in the console 
 
+const secondLetter = letters[1];
+console.log(secondLetter);
+
+
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
 
 const friends = [];
 
-// What a lonely array. Add at least 3 friend objects to it.  
+// What a lonely array. Add at least 3 friend objects to it.
+
+const friend1 = {
+  firstname: "Jack",
+  age: 46
+};
+
+const friend2 = {
+  firstname: "Jill",
+  age: 46
+};
+
+const friend3 = {
+  firstname: "Jules",
+  age: 16
+};
+
+friends.push(friend1, friend2, friend3);
+
+console.log(friends);
+
 
 
 // --------------------------------------
-// Exercise 3 - Get the index of first occurance of that value. 
+// Exercise 3 - Get the index of first occurrence of that value. 
 
 const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 
+
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
+
+const indexOfNumber = significantMathNumbers.indexOf(1729);
+
+console.log(indexOfNumber);
+
 
 
 // --------------------------------------
@@ -31,6 +61,8 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
 
+diet.splice(2, 0, 'hamburger', 'soda', 'pizza');
+console.log(diet);
 
 
 
@@ -40,7 +72,8 @@ const diet = ["tomato", "cucumber", "rocket"];
 // Remove the LAST element of the array.
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
 
-
+diet.pop();
+console.log(diet);
 
 
 
@@ -48,6 +81,9 @@ const diet = ["tomato", "cucumber", "rocket"];
 // Exercise 6 - Copy array
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
+
+const dinnerTray = diet;
+console.log(dinnerTray);
 
 
 
@@ -57,6 +93,10 @@ const diet = ["tomato", "cucumber", "rocket"];
 const lettersExpanded = ["a","b","c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
+
+for (let i = 1; i < lettersExpanded.length; i += 2) {
+  console.log(lettersExpanded[i]);
+}
 
 
 
@@ -70,5 +110,16 @@ const discardedNumbers = [];
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
 
+for (let i = 0; i < numbers.length; i++) {
+  const currentNumber = numbers[i];
+  
+  if(currentNumber > 6 || currentNumber < 0) {
+    console.log(currentNumber);
+  } else {
+    discardedNumbers.push(currentNumber);
+  }
+}
+
+console.log(discardedNumbers);
 // --------------------------------------
 

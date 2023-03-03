588450ec6530906003dde7914383afc5874e90ba
Jacob Uhlig <jacobuhlig@gmail.com>
2023-03-03 14:45:04 +0100

    Tried using modules instead of require, as well as using the path.resolve.

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
-const express = require("express");
-
+import express from "express";
 const app = express();
 
-app.use(express.static("public"));
+import path from "path";
 
+app.use(express.static("public"));
 
 // Pages
 
 app.get("/", (req, res) => {
-  res.send("Hello mister");
+  res.sendFile(path.resolve("public/index.html"));
   // res.sendFile(__dirname + "public/index.html");
 });
 
-
-
 // Time
 
-const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 // API
 
-app.get("/api/time", (req, res)  => {
-  res.send({data: weekdays[new Date().getDay()]});
+app.get("/api/time", (req, res) => {
+  res.send({ data: weekdays[new Date().getDay()] });
 });
-
-
 
 // port
 
 const PORT = 3000;
 
-app.listen(PORT, (error) => { // This is a way of error handling
-    if (error) { // the type of error is undefined
-        console.log(error);
-        return;
-    } else {
-        console.log(`Server is running on port ${PORT}`);
-    }
+app.listen(PORT, (error) => {
+  // This is a way of error handling
+  if (error) {
+    // the type of error is undefined
+    console.log(error);
+  }
+  console.log(`Server is running on port ${PORT}`);
 });

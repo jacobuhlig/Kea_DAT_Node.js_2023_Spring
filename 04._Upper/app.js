79d3fd455ef61bcbedee53a18a831ee30f1e5206
Jacob Uhlig <jacobuhlig@gmail.com>
2023-03-03 12:14:28 +0100

    Added some stuff.

--- 04._Upper/app.js
+++ 04._Upper/app.js
@@ -2,8 +2,24 @@
 import express from "express";
 const app = express();
 
+//The new way to do refer to the path
+import path from "path";
+
+// This is not necessarily the best method, because the user would then have to serve the actual path, every time a site, would want to be accessed
+// app.use(express.static("public"));
+
 // const jokes = require("./util/jokes"); //Omitting the extension ".js", is allowed, much to the chagrin of the Node developer
 // import jokes from "./util/jokes.js";
+
+const partOfPath = "public/pages/"; // could possibly result in another
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(partOfPath + "frontpage/frontpage.html"));
+});
+
+app.get("/quests", (req, res) => {
+  res.sendFile(path.resolve(partOfPath + "IRLQuests/IRLQuests.html"));
+});
 
 const PORT = 8080;
 app.listen(PORT, (error) => {
